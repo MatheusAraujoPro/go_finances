3497db8f535b4cd88d54bbf9c3832ef0c56e60eb
Matheus <matheusaraujo.ti10@gmail.com>
2021-11-23 06:59:20 -0300

    Listando os Totais por Categoria

--- src/Screens/Resume/index.tsx
+++ src/Screens/Resume/index.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HisotryCard } from '../../Components/HisotryCard';
 
-import { 
-Container,
-Header,
-Title
+import {
+    Container,
+    Header,
+    Title,
+    Content
 } from './styles';
+import { categories } from '../../Utils/categories';
 
-export function  Resume () {
-  return(
-      <Container>
-          <Header>
-              <Title>Resumo por Categoria</Title>
-          </Header>
-          
-          <HisotryCard
-            title="Compras"
-            amount="R$ 120,00"
-            color="red"
-          />
 
-      </Container>
-  )
+interface TransactionData {  
+    type: 'positive' | 'negative'
+    name: string,
+    amount: string,
+    category: string,
+    date: string
+}
+
+interface CategoryData {
+    key: string,
+    name: string,
+    total: string,
+    color: string
+}
+
+export function Resume() {
+    const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+    async function loadData() {
+        const dataKey = '@gofinances:transactions'
+        const response = await AsyncStorage.getItem(dataKey)
+        const responseFormatted = response ? JSON.parse(response) : []
+
+        //Pegando todas as transações de saída
+        const expensives = responseFormatted
+            .filter((expensive: TransactionData) => expensive.type === 'negative')
+
+        const totalByCategory: CategoryData[] = []
+
+        //Somando o valor de cada categoria
+        categories.forEach(category => {
+            let categorySum = 0
+
+            expensives.forEach((expensive: TransactionData) => {
+                if (expensive.category === category.key)
+                    categorySum += Number(expensive.amount)
+            })
+
+            if (categorySum > 0) {
+                const total = categorySum
+                    .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+
+                totalByCategory.push({
+                    key: category.key,
+                    name: category.name,
+                    total,
+                    color: category.color
+                })
+            }
+        })
+
+        setTotalByCategories(totalByCategory)
+
+    }
+
+    useEffect(() => {
+        loadData()
+    })
+    return (
+        <Container>
+            <Header>
+                <Title>Resumo por Categoria</Title>
+            </Header>
+
+            <Content>
+                {/* Como são poucas categorias o Método Map é pra esse caso, mais adequado */}
+                {totalByCategories.map(item => (
+                    <HisotryCard
+                        key={item.key}
+                        title={item.name}
+                        amount={item.total}
+                        color={item.color}
+                    />
+                ))}
+            </Content>
+
+        </Container>
+    )
 }
 
